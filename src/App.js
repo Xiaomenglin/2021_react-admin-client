be8aff8cb71bdf62eac26c000434e675a94836e7
xiaomenglin <xmlin3171@163.com>
2022-03-12 19:23:04 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
+import { Button , message} from 'antd';
 
 export default class App extends Component {
+  handleClick = ()=>{
+    message.success('成功了')
+  }
   render() {
     return (
-      <div>App</div>
+      <div>
+        <Button type="primary" onClick={this.handleClick}>Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <br />
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+      </div>
+      
+      
     )
   }
 }
