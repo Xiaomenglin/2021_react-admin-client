3cbbc4b9d4dd827b6b25f1d1dad4cd7730c1f149
xiaomenglin <xmlin3171@163.com>
2022-03-12 19:43:39 +0800

    update

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+//后台管理组件
+export default class admin extends Component {
+  render() {
+    return (
+      <div>admin</div>
+    )
+  }
+}
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+//登录路由组件
+export default class login extends Component {
+  render() {
+    return (
+      <div>login</div>
+    )
+  }
+}
